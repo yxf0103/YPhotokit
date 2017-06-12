2ead5cb91044de075bce8fbc13a9b51758d5744d
yxf <yanxuefeng@yxfdeMac-mini.local>
2017-06-12 11:19:33 +0800

    upload project

---
+++ DemoPhotoKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoPhotoKit
+//
+//  Created by yxf on 2017/6/12.
+//  Copyright © 2017年 yxf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoPhotoKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoPhotoKit
+//
+//  Created by yxf on 2017/6/12.
+//  Copyright © 2017年 yxf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
